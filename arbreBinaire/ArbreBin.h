ebb79d01ca2be257f7dbb5356902731ecfe67227
Rafael Masson <rmasson003@iutbayonne.univ-pau.fr>
2026-03-18 10:41:27 +0100

    ex1 arbre binaire : classe ArbreBin

---
+++ arbreBinaire/ArbreBin.h
@@ -0,0 +1,84 @@
+#ifndef ARBREBIN_H
+#define ARBREBIN_H
+
+template <class Element>
+class ArbreBin
+{
+protected:
+    Element m_e;
+    ArbreBin<Element> *m_fG;
+    ArbreBin<Element> *m_fD;
+
+public:
+    ArbreBin(const ArbreBin<Element> &);
+    ArbreBin<Element> &operator=(const ArbreBin<Element> &);
+    ArbreBin();
+    ~ArbreBin();
+    ArbreBin<Element> &fG() const;
+    ArbreBin<Element> &fD() const;
+    bool estVide() const;
+    Element racine() const;
+    void setRacine(Element);
+    void detruire();
+};
+
+/* ================================================================== */
+
+template <class Element>
+ArbreBin<Element>::ArbreBin() : m_e(), m_fG(nullptr), m_fD(nullptr) {}
+
+template <class Element>
+ArbreBin<Element>::~ArbreBin() {
+    // this->detruire(); // delete est en cascade donc pas obligatoire
+    delete m_fG; m_fG = nullptr;
+    delete m_fD; m_fD = nullptr;
+}
+
+template <class Element>
+ArbreBin<Element>& ArbreBin<Element>::fG() const {
+    return *(this->m_fG);
+}
+
+template <class Element>
+ArbreBin<Element>& ArbreBin<Element>::fD() const {
+    return *(this->m_fD);
+}
+
+template <class Element>
+bool ArbreBin<Element>::estVide() const
+{
+    return this->m_fG == nullptr; // verifie que le fils gauche car un arbre vide est (null,nullptr,nullptr)
+}
+
+template <class Element>
+Element ArbreBin<Element>::racine() const {
+    return this->m_e;
+}
+
+template <class Element>
+void ArbreBin<Element>::setRacine(Element elt)
+{
+    if (this->estVide())
+    {
+        this->m_fG = new ArbreBin<Element>();
+        this->m_fD = new ArbreBin<Element>();
+    }
+    this->m_e = elt;
+}
+
+template <class Element>
+void ArbreBin<Element>::detruire()
+// détruit tout le sous arbre et "retourne" un arbre vide
+{
+    if (!this->estVide())
+    {
+        this->fG().detruire();
+        this->fD().detruire();
+        delete this->m_fG;
+        this->m_fG = nullptr;
+        delete this->m_fD;
+        this->m_fD = nullptr;
+    }
+}
+
+#endif
---
+++ arbreBinaire/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+#include "ArbreBin.h"
+
+int main(void)
+{
+    cout << "--- TESTS ARBRE BINAIRE ---" << endl;
+    ArbreBin<int> abr;
+    cout << "Arbre vide ? " << (abr.estVide() ? "Oui" : "Non") << endl;
+    
+    abr.setRacine(8);
+    cout << "Arbre vide apres setRacine(8) ? " << (abr.estVide() ? "Oui" : "Non") << endl;
+    cout << "Racine : " << abr.racine() << endl;
+
+    // Ajout d'un sous-arbre
+    abr.fG().setRacine(4);
+    abr.fD().setRacine(12);
+
+    cout << "Fils gauche : " << abr.fG().racine() << endl;
+    cout << "Fils droit  : " << abr.fD().racine() << endl;
+
+    // Sous sous-arbres
+    abr.fG().fG().setRacine(2);
+    abr.fG().fD().setRacine(6);
+
+    cout << "Fils gauche du fils gauche : " << abr.fG().fG().racine() << endl;
+    cout << "Fils droit du fils gauche  : " << abr.fG().fD().racine() << endl;
+
+    return 0;
+}
